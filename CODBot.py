dc7bbd57fd8be3ed0e2e67be2c3857aa502ec062
Fernando <35590251+FernandoGuardado@users.noreply.github.com>
2021-01-21 01:47:20 -0700

    8s update

--- CODBot.py
+++ CODBot.py
@@ -3,6 +3,13 @@ import os
 import discord
 from discord.ext import commands
 
+def countMaps(lst, x): 
+    count = 0
+    for ele in lst: 
+        if (ele == x): 
+            count = count + 1
+    return count 
+
 client = commands.Bot(command_prefix = '!')
 
 @client.event
@@ -14,25 +21,50 @@ async def _8s(ctx):
     search = ['Raid', 'Crossroads', 'Moscow', 'Garrison', 'Miami', 'Checkmate']
     hardpoint = ['Crossroads', 'Checkmate', 'Garrison', 'Raid']
     control = ['Raid', 'Checkmate', 'Garrison']
+    mapPool = []
 
     firstMap = random.randint(0, 3)
+    mapPool.append(hardpoint[firstMap])
+
     secondMap = random.randint(0,5)
-    thirdMap = random.randint(0,2)
+    mapPool.append(search[secondMap])
+
+    while True:
+        thirdMap = random.randint(0,2)
+        mapCount = countMaps(mapPool, control[thirdMap])
+
+        if mapCount < 2:
+            mapPool.append(control[thirdMap])
+            break
+        else:
+            continue
 
     while True:
         fourthMap = random.randint(0, 3)
         if firstMap == fourthMap:
             continue
         else:
-            break
+            mapCount = countMaps(mapPool, hardpoint[fourthMap])
+
+            if mapCount < 2:
+                mapPool.append(hardpoint[fourthMap])
+                break
+            else:
+                continue
     while True:
         fifthMap = random.randint(0,5)
         if secondMap == fifthMap:
             continue
         else:
-            break
+            mapCount = countMaps(mapPool, search[fifthMap])
 
-    await ctx.send(f'{hardpoint[firstMap]}\n{search[secondMap]}\n{control[thirdMap]}\n{hardpoint[fourthMap]}\n{search[fifthMap]}')
+            if mapCount < 2:
+                mapPool.append(search[fifthMap])
+                break
+            else:
+                continue
+
+    await ctx.send(f'{mapPool[0]}\n{mapPool[1]}\n{mapPool[2]}\n{mapPool[3]}\n{mapPool[4]}')
 
 @client.command(aliases=['flip'])
 async def coinFlip(ctx):
